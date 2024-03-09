033c6f00fd4624fd2788c980d26bd4de2b6f1f62
Бронислав Scomi <cjlab4er@gmail.com>
2024-03-09 22:41:36 +0500

    HW Seminar1 maven

---
+++ Seminar1/maven-test/src/main/java/ru/gb/MavenClass.java
@@ -0,0 +1,10 @@
+package ru.gb;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MavenClass {
+    private String message = "Hello World!";
+}
---
+++ Seminar1/maven-test/src/main/java/ru/gb/MavenTestApp.java
@@ -0,0 +1,14 @@
+package ru.gb;
+
+
+/**
+ * Hello world!
+ */
+public class MavenTestApp {
+
+    public static void main(String[] args) {
+        MavenClass mavenClass = new MavenClass();
+        System.out.println(mavenClass.getMessage());
+    }
+
+}
