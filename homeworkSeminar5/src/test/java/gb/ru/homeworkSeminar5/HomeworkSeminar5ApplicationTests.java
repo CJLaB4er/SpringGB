9daa9909d08de48ec6ded2ced630cb1f3648ed35
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-04-04 22:29:05 +0300

    start HW sem5

---
+++ homeworkSeminar5/src/test/java/gb/ru/homeworkSeminar5/HomeworkSeminar5ApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.ru.homeworkSeminar5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSeminar5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
